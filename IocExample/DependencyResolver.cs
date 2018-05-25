8d949ed634b6324ae0c6d4e914b60a18878e8576
Navras Bokiev <navras96@gmail.com>
2018-05-26 00:01:07 +0500

    first commit

---
+++ IocExample/DependencyResolver.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace IocExample
+{
+    class DependencyResolver
+    {
+        private readonly Dictionary<Type, Tuple<Type, Dictionary<string, object>>> dep;
+
+        public DependencyResolver()
+        {
+            dep = new Dictionary<Type, Tuple<Type, Dictionary<string, object>>>();
+        }
+
+        public TType Get<TType>() where TType : class
+        {
+            return (TType) Get(typeof(TType));
+        }
+
+    public object Get(Type type) 
+        {
+            var constructorInfo = Utils.GetSingleConstructor(dep[type].Item1);
+            var parameters = constructorInfo.GetParameters();
+
+            if (parameters.Length == 0)
+                return Utils.CreateInstance(dep[type].Item1);
+
+            List<object> arguments = new List<object>();
+            foreach (ParameterInfo parameter in parameters)
+            {
+                arguments.Add(dep[type].Item2.ContainsKey(parameter.Name)
+                    ? dep[type].Item2[parameter.Name]
+                    : Get(parameter.ParameterType));
+            }
+
+            return Utils.CreateInstance(dep[type].Item1, arguments);
+        }
+
+        public ClassBinder Bind<TType>() where TType : class
+        {
+            return new ClassBinder(dep, typeof(TType));
+        }
+    }
+
+    public class ClassBinder
+    {
+        private readonly Dictionary<Type, Tuple<Type, Dictionary<string, object>>> dep;
+        private readonly Type key;
+        public ClassBinder(Dictionary<Type, Tuple<Type, Dictionary<string, object>>> dep, Type key)
+        {
+            this.dep = dep;
+            this.key = key;
+        }
+        public ArgumentsBinder To<TType>() where TType : class
+        {
+            dep[key] = new Tuple<Type, Dictionary<string, object>>(
+                typeof(TType), 
+                new Dictionary<string, object>());
+
+            return new ArgumentsBinder(dep, key);
+        }
+        public ArgumentsBinder ToSelf()
+        {
+            dep[key] = new Tuple<Type, Dictionary<string, object>>(
+                key, new Dictionary<string, object>());
+
+            return new ArgumentsBinder(dep, key);
+        }
+
+    }
+
+    public class ArgumentsBinder
+    {
+        private readonly Dictionary<Type, Tuple<Type, Dictionary<string, object>>> dep;
+        private readonly Type _key;
+        public ArgumentsBinder(Dictionary<Type, Tuple<Type, Dictionary<string, object>>> dep, Type key)
+        {
+            this.dep = dep;
+            this._key = key;
+        }
+
+        public ArgumentsBinder WithConstructorArgument(string name, object value)
+        {
+            dep[_key].Item2[name] = value;
+            return this;
+        }
+    }
+}
+
---
+++ IocExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using IocExample.Classes;
+using Ninject;
+
+namespace IocExample
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    var logger = new ConsoleLogger();
+        //    var sqlConnectionFactory = new SqlConnectionFactory("SQL Connection", logger);
+        //    var createUserHandler = new CreateUserHandler(new UserService(new QueryExecutor(sqlConnectionFactory), new CommandExecutor(sqlConnectionFactory), new CacheService(logger, new RestClient("API KEY"))), logger);
+
+        //    createUserHandler.Handle();
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    IKernel idiKernel = new StandardKernel();
+
+        //    idiKernel.Bind<ILogger>().To<ConsoleLogger>();
+        //    idiKernel.Bind<IConnectionFactory>()
+        //        .To<SqlConnectionFactory>()
+        //        .WithConstructorArgument("sqlConnection", "SQL Connection");
+        //    idiKernel.Bind<UserService>().ToSelf();
+        //    idiKernel.Bind<QueryExecutor>().ToSelf();
+        //    idiKernel.Bind<RestClient>().ToSelf().WithConstructorArgument("apiKey", "API KEY");
+        //    idiKernel.Bind<CacheService>().ToSelf();
+        //    idiKernel.Bind<CommandExecutor>().ToSelf();
+        //    idiKernel.Bind<CreateUserHandler>().ToSelf();
+
+        //    var createUserHandler = idiKernel.Get<CreateUserHandler>();
+        //    createUserHandler.Handle();
+        //    Console.ReadKey();
+        //}
+
+        static void Main(string[] args)
+        {
+            var di = new DependencyResolver();
+
+            di.Bind<ILogger>().To<ConsoleLogger>();
+            di.Bind<IConnectionFactory>()
+                .To<SqlConnectionFactory>()
+                .WithConstructorArgument("sqlConnection", "SQL Connection");
+            di.Bind<UserService>().ToSelf();
+            di.Bind<QueryExecutor>().ToSelf();
+            di.Bind<RestClient>().ToSelf().WithConstructorArgument("apiKey", "API KEY");
+            di.Bind<CacheService>().ToSelf();
+            di.Bind<CommandExecutor>().ToSelf();
+            di.Bind<CreateUserHandler>().ToSelf();
+
+            var createUserHandler = di.Get<CreateUserHandler>();
+            createUserHandler.Handle();
+            Console.ReadKey();
+        }
+    }
+}
